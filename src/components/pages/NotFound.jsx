ff1b5fc9dc21897ca2678c382f18947ca34f96c0
noelbjohn <noel@reviei.com>
2021-06-19 17:09:15 +0530

    Added NotFound page component.

---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="alert alert-danger w-75 text-center" role="alert">
+    <h1>404 Not Found</h1>
+  </div>
+);
+
+export default NotFound;
